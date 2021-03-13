dcc3e9e5dbdbbaa18e18d3bca5b608a24dc32147
Selino Valdes <selinov@gmail.com>
2021-03-12 18:10:38 -0800

    Adding some grafx.

--- src/tokens/colors.js
+++ src/tokens/colors.js
@@ -22,16 +22,17 @@ const colors = {
  "successHover": "rgba(37, 149, 62, 1)",
  "successInactive": "rgba(40, 167, 69, 1)",
  "brandLeadRoi": "rgba(222, 140, 8, 1)",
- "brandLeadGen": "rgba(0, 96, 156, 1)",
+ "brandLeadGen": "rgba(255, 255, 255, 1)",
  "brandLeadNav": "rgba(76, 157, 47, 1)",
  "graysGray5": "rgba(242, 242, 242, 1)",
  "graysGray4": "rgba(224, 224, 224, 1)",
  "graysGray3": "rgba(189, 189, 189, 1)",
  "graysGray2": "rgba(130, 130, 130, 1)",
+ "graysGray6": "rgba(66, 66, 66, 1)",
  "graysGray1": "rgba(66, 66, 66, 1)",
- "background": "rgba(244, 244, 244, 1)",
- "white": "rgba(255, 255, 255, 1)",
- "black": "rgba(0, 42, 63, 1)"
+ "background": "rgba(238, 238, 238, 1)",
+ "white": "rgba(0, 96, 156, 1)",
+ "black": "rgba(51, 51, 51, 1)"
 }
 
 module.exports = colors;
--- src/tokens/shadows.js
+++ src/tokens/shadows.js
@@ -4,7 +4,7 @@ const shadows = {
  "deepMulti": "0px 4px 20px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.17)",
  "deep": "3px 3px 3px rgba(196, 196, 196, 0.75)",
  "medium": "0px 0px 5px rgba(0, 0, 0, 0.5)",
- "soft": "0px 0px 5px rgba(196, 196, 196, 1)"
+ "soft": "1px 2px 8px rgba(196, 196, 196, 0.5)"
 }
 
 module.exports = shadows;
