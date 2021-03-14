dfba8493f25518ce020fd80ac782c60d4246f36b
Selino Valdes <selinov@gmail.com>
2021-03-14 16:58:06 -0700

    [ch932] Hiding the Clariture logo on smaller displays.

--- src/components/Nav/NavHeader.js
+++ src/components/Nav/NavHeader.js
@@ -4,7 +4,6 @@ import { ReactComponent as LogoChText } from "../../graphics/logoChText.svg"
 import { ReactComponent as LogoAppName } from "../../graphics/logoLeadnav.svg"
 import { ReactComponent as NavLocation } from "../../graphics/icoLocation.svg"
 import { ReactComponent as NavHamburger } from "../../graphics/icoHamburger.svg"
-import { ReactComponent as NavChevronDown } from "../../graphics/icoChevronD.svg"
 import { ReactComponent as VerticalRule } from "../../graphics/verticalRule.svg"
 import CompanyDropdown from "./CompanyDropdown"
 
@@ -13,15 +12,14 @@ export default function NavHeader(props) {
     <nav className='p-2 mx-auto bg-brand text-white flex justify-between'>
       <div className='my-auto'>
         <Logo className='inline-block mr-2 fill-current' />
-        <LogoChText className='inline-block' />
-        <VerticalRule className='inline-block opacity-50 mx-2' />
+        <LogoChText className='inline-block hidden tablet:inline' />
+        <VerticalRule className='hidden tablet:inline opacity-50 mx-2' />
         <LogoAppName className='inline-block' />
       </div>
       <div className='nav-control flex flex-row'>
         <CompanyDropdown className='mr-4' />
         <NavLocation className='svg-light mr-4 my-auto' />
         <NavHamburger className='svg-light mr-4 my-auto' />
-        <NavChevronDown className='svg-light my-auto' />
       </div>
     </nav>
   )
