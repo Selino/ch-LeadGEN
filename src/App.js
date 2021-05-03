d91d63034581074b924edd5ccfc4bfd7ad129b56
Selino Valdes <selinov@gmail.com>
2021-05-03 12:19:27 -0700

    [ch1256] Setting props to use numbers instead of strings for metric cards.

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
         <div className='mb-4 grid gap-7 grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-4'>
           <MetricCard
             currency='USD'
-            value='19.8'
-            baseValue='19.5'
+            value={19.8}
+            baseValue={19.5}
             label='Spend to Date'
             unit='thousand'
           />
-          <MetricCard value='200' baseValue='100' label='Conversions' />{" "}
-          <MetricCard currency='USD' value='205' baseValue='205' label='CPC' />{" "}
-          <MetricCard value='92' label='Live' />
+          <MetricCard value={200} baseValue={100} label='Conversions' />{" "}
+          <MetricCard currency='USD' value={205} baseValue={205} label='CPC' />{" "}
+          <MetricCard value={92} label='Live' />
         </div>
         <ExecutiveSummary />
       </main>
