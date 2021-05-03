5061f2ec98ced8d108f44fd84463bd4cfc878ef1
Selino Valdes <selinov@gmail.com>
2021-05-03 11:47:00 -0700

    [ch1251] Switching dropdowns to react-bootstrap.

--- src/components/Nav/NavHeader.js
+++ src/components/Nav/NavHeader.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Navbar, NavDropdown, Button } from "react-bootstrap"
 import { ReactComponent as Logo } from "../../graphics/logoHexagons.svg"
 import { ReactComponent as LogoChText } from "../../graphics/logoChText.svg"
 import { ReactComponent as LogoAppName } from "../../graphics/logoLeadnav.svg"
@@ -9,18 +10,58 @@ import CompanyDropdown from "./CompanyDropdown"
 
 export default function NavHeader(props) {
   return (
-    <nav className='p-2 mx-auto bg-brand text-white flex justify-between'>
-      <div className='my-auto'>
-        <Logo className='inline-block mr-2 fill-current' />
-        <LogoChText className='hidden tablet:inline' />
-        <VerticalRule className='hidden tablet:inline opacity-50 mx-2' />
-        <LogoAppName className='inline-block' />
-      </div>
-      <div className='nav-control flex flex-row'>
-        <CompanyDropdown className='mr-4' />
-        <NavLocation className='svg-light mr-4 my-auto' />
-        <NavHamburger className='svg-light mr-2 my-auto' />
-      </div>
-    </nav>
+    <Navbar
+      fixed='top'
+      className='p-2 mx-auto bg-brand text-white flex justify-between'
+    >
+      <Navbar.Brand>
+        <div>
+          <Logo className='inline-block mr-2 fill-current' />
+          <LogoChText className='hidden tablet:inline' />
+          <VerticalRule className='hidden tablet:inline opacity-50 mx-2' />
+          <LogoAppName className='inline-block' />
+        </div>
+      </Navbar.Brand>
+
+      <Navbar.Collapse className='justify-content-end'>
+        <NavDropdown
+          title={<CompanyDropdown className='svg-light' />}
+          id='nav-company-dropdown'
+        >
+          <NavDropdown.Item eventKey='4.1'>Action</NavDropdown.Item>
+          <NavDropdown.Item eventKey='4.2'>Another action</NavDropdown.Item>
+          <NavDropdown.Item eventKey='4.3'>
+            Something else here
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item eventKey='4.4'>Separated link</NavDropdown.Item>
+        </NavDropdown>
+
+        <NavDropdown
+          alignRight
+          title={<NavLocation className='svg-light' />}
+          id='nav-facility-dropdown'
+        >
+          <NavDropdown.Item eventKey='4.1'>
+            Mountain View Regional Medical Center
+          </NavDropdown.Item>
+          <NavDropdown.Item eventKey='4.2'>Another action</NavDropdown.Item>
+          <NavDropdown.Item eventKey='4.3'>
+            Something else here
+          </NavDropdown.Item>
+        </NavDropdown>
+
+        <NavDropdown
+          alignRight
+          title={<NavHamburger className='svg-light' />}
+          id='nav-user-dropdown'
+        >
+          <NavDropdown.Item eventKey='4.1'>Campaign Overview</NavDropdown.Item>
+          <NavDropdown.Item eventKey='4.2'>Executive Summary</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item eventKey='4.4'>Log Out</NavDropdown.Item>
+        </NavDropdown>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
