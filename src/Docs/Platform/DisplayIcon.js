bee504b937589a1669dce3d4ec356732a312f33b
Selino Valdes <selinov@gmail.com>
2021-05-11 21:29:52 -0700

    [ch1287] Refining copy and layout on icons loader.

--- src/Docs/Platform/DisplayIcon.js
+++ src/Docs/Platform/DisplayIcon.js
@@ -24,7 +24,7 @@ const ShowAllIcons = () => {
   ]
 
   return (
-    <div className='mb-4 grid gap-7 grid-cols-1 mobile:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4'>
+    <div className='mb-4 grid gap-7 grid-cols-1 mobile:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-5 mt-3'>
       {myArray.map((a) => {
         return <DisplayIcon name={a} />
       })}
