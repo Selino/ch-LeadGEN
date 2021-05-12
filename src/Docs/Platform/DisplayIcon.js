d246a499c4543b07f899f500ee0bb65b73558fac
Selino Valdes <selinov@gmail.com>
2021-05-12 13:07:18 -0700

    [end ch1287] Breaking icons in utils and docs with anchor links.

--- src/Docs/Platform/DisplayIcon.js
+++ src/Docs/Platform/DisplayIcon.js
@@ -1,43 +1,50 @@
 import React from "react"
 import { ReactSVG } from "react-svg"
 
-function DisplayIcon({ name = "icoCalendar", styled = "h-8" }) {
+function DisplayIcon({ name }) {
   return (
-    <div className=''>
+    <div key={name}>
       <ReactSVG src={"/graphics/" + name + ".svg"} className='svg-xl' />
       <div className='text-center text-sm text-brand mt-1'>{name}</div>
     </div>
   )
 }
 
-const ShowAllIcons = () => {
-  const myArray = [
-    "icoArrowCircleDown",
-    "icoArrowCircleUp",
-    "icoCalendar",
-    "icoCarretDown",
-    "icoCheck",
-    "icoCheckSquare",
-    "icoChevronL",
-    "icoChevronR",
-    "icoCloseX",
-    "icoCloudDownload",
-    "icoEqualCircle",
-    "icoExclamationTriangle",
-    "icoGripVertical",
-    "icoHamburger",
-    "icoInfoCircle",
-    "icoLocation",
-    "icoPhone",
-    "icoSearch",
-    "icoSliders",
-    "icoSquare",
-  ]
-
+const ShowAllIcons = ({ type }) => {
+  let myArray = []
+  switch (type) {
+    default:
+      myArray = [
+        "icoArrowCircleDown",
+        "icoArrowCircleUp",
+        "icoCalendar",
+        "icoCarretDown",
+        "icoCheck",
+        "icoCheckSquare",
+        "icoChevronL",
+        "icoChevronR",
+        "icoCloseX",
+        "icoCloudDownload",
+        "icoEqualCircle",
+        "icoExclamationTriangle",
+        "icoGripVertical",
+        "icoHamburger",
+        "icoInfoCircle",
+        "icoLocation",
+        "icoPhone",
+        "icoSearch",
+        "icoSliders",
+        "icoSquare",
+      ]
+      break
+    case "docs":
+      myArray = ["docExcel", "docPdf"]
+      break
+  }
   return (
     <div className='mb-4 grid gap-7 grid-cols-1 mobile:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-5 mt-3'>
       {myArray.map((a) => {
-        return <DisplayIcon name={a} />
+        return <DisplayIcon key={a} name={a} />
       })}
     </div>
   )
