4bdde42df39a0f5c18b3ac606dcad5369d69ccd5
Selino Valdes <selinov@gmail.com>
2021-06-14 07:59:08 -0700

    [ch1365] Moving controls into same file for KMOT graph.

---
+++ src/components/Dashboard/ConversionsByType.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function ConversionsByType() {
+  return <div>stuff</div>
+}
--- src/components/Dashboard/KeyMetricsGraph.js
+++ src/components/Dashboard/KeyMetricsGraph.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { ReactSVG } from "react-svg"
 import {
   ResponsiveContainer,
   LineChart,
@@ -13,7 +14,48 @@ import { format, parseISO } from "date-fns"
 import colors from "tokens/colors"
 import borderWidths from "tokens/borderWidths"
 import fontSizes from "tokens/fontSizes"
-import KMOTControls from "components/Dashboard/KeyMetricsControls"
+import { Dropdown } from "react-bootstrap"
+
+export const KMOTControls = () => {
+  return (
+    <div className='sm:flex justify-between gap-4'>
+      <div className='sm:flex'>
+        <Dropdown className=''>
+          <Dropdown.Toggle variant='' id='dropdown-basic'>
+            All Channels
+            <ReactSVG
+              src='/graphics/utiCarretDown.svg'
+              className='svg-primary inline-block align-middle'
+            />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href='#/action-1'>All Channels</Dropdown.Item>
+            <Dropdown.Item href='#/action-2'>Facebook</Dropdown.Item>
+            <Dropdown.Item href='#/action-3'>Google Ads</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown className=''>
+          <Dropdown.Toggle variant='' id='dropdown-basic'>
+            Clicks & Impressions
+            <ReactSVG
+              src='/graphics/utiCarretDown.svg'
+              className='svg-primary inline-block align-middle'
+            />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href='#/action-1'>
+              Clicks & Impressions
+            </Dropdown.Item>
+            <Dropdown.Item href='#/action-2'>CPC & Spend</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  )
+}
 
 export default function KMOT({ data }) {
   return (
